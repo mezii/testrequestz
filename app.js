478c7e2a4e583d41bc435b6f49568ddd8e14dcb0
mezii <deptrai.team.pro@gmail.com>
2024-12-05 01:59:24 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const axios = require('axios');
+
+axios.get("http://localhost:3000", async  (req, res) => {
+    console.log("Sent")
+})
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+// Middleware to log all headers
+app.use((req, res, next) => {
+  console.log('Incoming Request Headers:', req.headers);
+  next(); // Pass control to the next middleware or route handler
+});
+
+// A sample route
+app.get('/', (req, res) => {
+  res.send('Check your console for logged headers.');
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
